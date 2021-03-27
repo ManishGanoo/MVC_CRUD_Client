85d209092f7f0921af00ea72a8b4853a103814f5
Manish Ganoo <manish.ganoo@gmail.com>
2021-03-27 15:58:14 +0400

    Create SocieteModel.cs

---
+++ SocieteModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DdLibrary.Models
+{
+    public class SocieteModel
+    {
+        public int SocieteId { get; set; }
+        public string DetailSociete { get; set; }
+        public string SocieteAdresse { get; set; }
+        public string ContactPerson { get; set; }
+    }
+}
